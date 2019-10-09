cae8dd1ee764c9a0d6dddcf536e021445d62c792
Chris Durden <cdurden@gmail.com>
2019-10-09 10:43:34 -0500

    Adding Assignment route

--- algebra742.py
+++ algebra742.py
@@ -246,7 +246,7 @@ def Assignment(lti=lti, assignment=None,q=None,i=None):
     #user = User(username="test user", lti_user_id="asdf")
     try:
         q = int(q)
-    except:
+    except KeyError:
         for j,QuestionData in enumerate(QuestionSets[assignment]):
             for k in range(len(QuestionData['ParameterSetVariants'])):
                 statement = select([question_scores,Question.__table__]).where(and_(question_scores.c.user_id==user.id, question_scores.c.question_id==Question.__table__.c.id, Question.__table__.c.assignment==assignment, Question.__table__.c.number==j+1, Question.__table__.c.variant_index==k, question_scores.c.score==1))
