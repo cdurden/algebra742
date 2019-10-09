2604bd225539d1346f0f286ef0a34314e4f0b700
Chris Durden <cdurden@gmail.com>
2019-10-09 11:04:22 -0500

    Adding Assignment route

--- algebra742.py
+++ algebra742.py
@@ -251,7 +251,7 @@ def Assignment(lti=lti, assignment=None,q=None,i=None):
             for k in range(len(QuestionData['ParameterSetVariants'])):
                 statement = select([question_scores,Question.__table__]).where(and_(question_scores.c.user_id==user.id, question_scores.c.question_id==Question.__table__.c.id, Question.__table__.c.assignment==assignment, Question.__table__.c.number==j+1, Question.__table__.c.variant_index==k, question_scores.c.score==1))
                 results = db.session.execute(statement).first()
-                app.logger.debug("j={:d}, len(results)={:d}".format(k,len(results)))
+                app.logger.debug("j={:d}, len(results)={:s}".format(k,results))
                 if not results:
                     q = j+1
                     i = k
