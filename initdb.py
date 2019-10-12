9021d95fca98b3bd37ef781089e6b90baf358291
Chris Durden <cdurden@gmail.com>
2019-10-12 15:58:53 -0500

    switching to mysql

--- initdb.py
+++ initdb.py
@@ -1,5 +1,6 @@
-from algebra742 import db
-connection = db.engine.raw_connection()
-cursor = connection.cursor()
-cursor.execute("CREATE DATABASE IF NOT EXISTS algebra742;")
+from algebra742 import db,app
+from sqlalchemy_utils import create_database, database_exists
+url = app.config['SQLALCHEMY_DATABASE_URI']
+if not database_exists():
+    create_database(url)
 db.create_all()
