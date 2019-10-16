0e785412b078c3f59acf4973c418d12dadfe5b28
Chris Durden <cdurden@gmail.com>
2019-10-16 10:39:06 -0500

    Solve Equations Guided

--- algebra742.py
+++ algebra742.py
@@ -152,8 +152,8 @@ class SolveEquationStepForm(Form):
 
     :param Form:
     """
-    #operation = RadioField('operation', choices=[('+','add'),('-','subtract'),('*','multiply'),('/','divide')])
-    operation = RadioField('operation', choices=[('+','$+$'),('-','$-$'),('*',r'$\times$'),('/',r'$\div$')])
+    operation = RadioField('operation', choices=[('+','add'),('-','subtract'),('*','multiply'),('/','divide')])
+    #operation = RadioField('operation', choices=[('+','$+$'),('-','$-$'),('*',r'$\times$'),('/',r'$\div$')])
     operand = StringField('operand')
     new_equation = StringField('new_equation')
 
