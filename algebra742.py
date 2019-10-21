ff22833b48e047d9100764609d3ca49bb709e0be
Chris Durden <cdurden@gmail.com>
2019-10-21 07:09:17 -0500

    CommunicatingReasoning

--- algebra742.py
+++ algebra742.py
@@ -415,6 +415,10 @@ def Assignment(lti=lti, assignment=None,q=None,i=None):
         correct = True
         #lhs,rhs = QuestionData['ParameterSetVariants'][i]['equation'].split("=")
         lhs,rhs = Parameters['equation'].split("=")
+        if QuestionData['Type'] == 'SetUpAndSolveEquationGuided':
+            for it,variable in enumerate(Parameters['variables']):
+                lhs = lhs.replace(variable, form.equation_form.variables[it].data)
+                rhs = rhs.replace(variable, form.equation_form.variables[it].data)
         #QuestionData['ParameterSetVariants'][i]['equation_latex'] = "{:s}={:s}".format(latex(parse_expr(lhs, transformations=transformations)),latex(parse_expr(rhs, transformations=transformations)))
         Parameters['equation_latex'] = "{:s}={:s}".format(latex(parse_expr(lhs, transformations=transformations)),latex(parse_expr(rhs, transformations=transformations)))
         it = 0
