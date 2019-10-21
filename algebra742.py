5e157054a51055e3709e6edaa1d8bfad1e3f355f
Chris Durden <cdurden@gmail.com>
2019-10-21 10:51:23 -0500

    CommunicatingReasoning

--- algebra742.py
+++ algebra742.py
@@ -479,14 +479,16 @@ def Assignment(lti=lti, assignment=None,q=None,i=None):
                     message = "Nice work! Keep going until you find the value of the variable!"
                     correct = False
                 elif correct:
-                    answer = stepform.new_equation.data
+                    message = "Your answer {:s} is correct".format(stepform.new_equation.data)
+                    #answer = stepform.new_equation.data
             if QuestionData['Type'] == 'SetUpAndSolveEquationGuided':
                 if correct and (simplify(parse_expr(new_lhs, transformations=transformations)-parse_expr(form.equation_form.variables[0].data, transformations=transformations))!=0 and simplify(parse_expr(new_rhs, transformations=transformations)-parse_expr(form.equation_form.variables[0].data, transformations=transformations))!=0):
                     form.steps.append_entry()
                     message = "Nice work! Keep going until you find the value of the variable!"
                     correct = False
                 elif correct:
-                    answer = stepform.new_equation.data
+                    #answer = stepform.new_equation.data
+                    message = "Your answer {:s} is correct".format(stepform.new_equation.data)
         except:
             correct = False
         answer = json.dumps(form.data)
