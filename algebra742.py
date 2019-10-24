b6bb87ee459a3b4fdd92c81ab7010971bbc68692
Chris Durden <cdurden@gmail.com>
2019-10-24 07:03:25 -0500

    Simplify

--- algebra742.py
+++ algebra742.py
@@ -380,7 +380,7 @@ def Assignment(lti=lti, assignment=None,q=None,i=None):
         form = ExpressionForm()
         if form.data == None:
             app.logger.error("no form data")
-            statement = select([question_scores,Question.__table__]).where(and_(question_scores.c.user_id==user.id, question_scores.c.question_id==Question.__table__.c.id, Question.__table__.c.number==q, Question.__table__.c.variant_index=i))
+            statement = select([question_scores,Question.__table__]).where(and_(question_scores.c.user_id==user.id, question_scores.c.question_id==Question.__table__.c.id, Question.__table__.c.number==q, Question.__table__.c.variant_index==i))
             results = db.session.execute(statement).first()
             form.data = json.loads(results.answer)
         try:
