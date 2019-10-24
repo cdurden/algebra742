d85b2d9652aca35a6dcfcde068c457112c43e828
Chris Durden <cdurden@gmail.com>
2019-10-24 07:14:12 -0500

    Simplify

--- algebra742.py
+++ algebra742.py
@@ -384,6 +384,7 @@ def Assignment(lti=lti, assignment=None,q=None,i=None):
             try:
                 statement = select([question_scores,Question.__table__]).where(and_(question_scores.c.user_id==user.id, question_scores.c.question_id==Question.__table__.c.id, Question.__table__.c.number==q, Question.__table__.c.variant_index==i))
                 results = db.session.execute(statement).first()
+                app.logger.error(results)
                 form.data = json.loads(results.answer)
             except IOError:
                 pass
