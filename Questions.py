18c410a052146c4c59395c2db71a596e628850c5
Chris Durden <cdurden@gmail.com>
2019-10-30 11:43:23 -0500

    update

--- Questions.py
+++ Questions.py
@@ -1,4 +1,26 @@
 QuestionSets = {
+    'ClassworkOct30B': {
+        'ProvideImmediateFeedback': True,
+        'Questions':
+        [
+            {
+            'Type': 'SolveEquationGuided',
+            'Template': 'SolveEquationGuided.html',
+            'ParameterSetVariants': [
+                {'equation': '1/2(4a-8)=-2(2+a)', 'equation_latex': r'\frac{1}{2}(4a-8)=-2(2+a)', 'variables': ['a']},
+                {'equation': '4(2x-1)=4x+6', 'variables': ['x']},
+                # You and Review (2 min + 2 min)
+                {'equation': '8s-10=3(6-2s)', 'variables': ['s']},
+                {'equation': '6(5m-3)=(1/3)*(24m+12)', 'equation_latex': r'6(5m-3)=\frac{1}{3}(24m+12)', 'variables': ['m']},
+                {'equation': '-2y-4=2(6-2y)', 'equation_latex': r'-2y-4=2(6-2y)', 'variables': ['y']},
+                {'equation': '3(2t-2)-26=2(2-t)', 'variables': ['t']},
+                # We (2 min)
+                {'equation': '7(n-1)=-2(3+n)', 'equation_latex': '7(n-1)=-2(3+n)', 'variables': ['n']},
+                ],
+            'SpaceAfter': '4cm',
+            }
+        ]
+    },
     'ClassworkOct30': {
         'ProvideImmediateFeedback': True,
         'Questions':
