35d9b8f8a75fb331087f9a4967888ead89b1ffd7
Chris Durden <cdurden@gmail.com>
2019-11-14 22:32:09 -0600

    coordinate.pairs

--- algebra742.py
+++ algebra742.py
@@ -453,7 +453,6 @@ def Assignment(lti=lti, assignment=None,q=None,i=None):
             correct = input_set_of_coordinate_pairs == Params['set_of_coordinate_pairs']
             if not correct:
                 message = "Coordinate pairs {:s} are not correct.".format(Params['set_of_coordinate_pairs'] - input_set_of_coordinate_pairs)
-                break
         except ValueError:
             message = "Coordinate pairs could not be read. Make sure that you entered them correctly."
             correct = False
