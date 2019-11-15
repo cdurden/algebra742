8614e417d57e54bf2163ab7a543f316dcdf67bf2
Chris Durden <cdurden@gmail.com>
2019-11-14 22:41:43 -0600

    coordinate.pairs

--- algebra742.py
+++ algebra742.py
@@ -449,7 +449,7 @@ def Assignment(lti=lti, assignment=None,q=None,i=None):
             input_set_of_coordinate_pairs = set()
             for input_coordinate_pair_string in input_coordinate_pairs:
                 input_coordinate_pair = tuple(int(x.strip("{()} ")) for x in input_coordinate_pair_string.split(","))
-                set_of_coordinate_pairs.add(input_coordinate_pair)
+                input_set_of_coordinate_pairs.add(input_coordinate_pair)
             correct = input_set_of_coordinate_pairs == Params['set_of_coordinate_pairs']
             if not correct:
                 message = "Coordinate pairs {:s} are not correct.".format(Params['set_of_coordinate_pairs'] - input_set_of_coordinate_pairs)
