d18255ee2d7732bc33693d03301e54366e1ff4f9
Chris Durden <cdurden@gmail.com>
2019-11-16 18:07:47 -0600

    SolvingEquationsTest

--- Questions.py
+++ Questions.py
@@ -183,7 +183,7 @@ QuestionSets = {
             'Template': 'Empty.html',
             'ParameterSetVariants': [
                 {'image': None,
-                    'choices': [('a', 'Using zero pairs and reciprocal pairs to solve equations'), ('b', 'Eliminating variable terms when solving equations'), ('c', 'The distributive property'), ('d', 'Understanding like terms'), ('e', 'Solving equations involving absolute value'), ('f', 'Solving equations with variables on both sides'), ('g', 'Combining like terms when solving equations')],
+                    'choices': [('a', 'Using zero pairs and reciprocal pairs to solve equations'), ('b', 'Eliminating variable terms when solving equations'), ('c', 'The distributive property'), ('d', 'Understanding like terms'), ('e', 'Solving equations involving absolute value'), ('f', 'Solving equations with variables on both sides'), ('g', 'Combining like terms when solving equations'), ('h', 'Solving proportions and other equations with quotients')],
                     'question': "The main learning goals that you need to work on are ",
                     'CorrectAnswer': None,
                     },
