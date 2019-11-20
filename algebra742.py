41aff57099a7f1ac695de651164797d5f1830f66
Chris Durden <cdurden@gmail.com>
2019-11-20 00:36:27 -0600

    RepresentRelations

--- algebra742.py
+++ algebra742.py
@@ -394,7 +394,7 @@ def plot_mapping_diagram(N=5, seed=0):
     inputs = list(set(x))
     outputs = list(set(y))
     axis = fig.add_subplot(1, 1, 1)
-    axis.scatter([(-i,-1) for i in range(len(inputs)+1)]+[(-j,4) for j in range(len(outputs)+1)])
+    axis.scatter([(-i,-1) for i in range(len(inputs)+1)],[(-j,4) for j in range(len(outputs)+1)])
     for i in range(len(inputs)):
         #axis.annotate(str(inputs[i]),(-i,0))
         axis.text(-i,0,str(inputs[i]))
