6982d902e10eb21845ff3ae14c148eeaf60c3f46
Chris Durden <cdurden@gmail.com>
2019-11-25 00:08:50 -0600

    memory

--- static/js/connect4.js
+++ static/js/connect4.js
@@ -121,7 +121,7 @@ function update_game(flipped_cards, players, active_player, dice, selectable_car
     die2_div = document.getElementById("die2");
     if (dice[0] != null && dice[1] != null) {
         die1_div.innerHTML = dice[0].toString();
-        die1_div.innerHTML = dice[1].toString();
+        die2_div.innerHTML = dice[1].toString();
     }
     players_dashboard = document.getElementById("players-dashboard");
     //players_dashboard.innerHTML = '';
