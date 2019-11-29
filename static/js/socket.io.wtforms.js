5967430f9e22bdb0190b05f8e9874d0e402c4097
Chris Durden <cdurden@gmail.com>
2019-11-28 22:28:56 -0600

    Question

--- static/js/socket.io.wtforms.js
+++ static/js/socket.io.wtforms.js
@@ -1,8 +1,6 @@
-<script>
-    $(document).ready(function() {
-        $('form').submit(function (e) {
-            socket.emit('form_submit', data=$('form').serialize());
-            e.preventDefault(); // block the traditional submission of the form.
-        });
+$(document).ready(function() {
+    $('form').submit(function (e) {
+        socket.emit('form_submit', data=$('form').serialize());
+        e.preventDefault(); // block the traditional submission of the form.
     });
-</script>
+});
