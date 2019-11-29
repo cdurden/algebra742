f225530b8ffe01da52fcc8c18482a0f0de39042b
Chris Durden <cdurden@gmail.com>
2019-11-28 21:56:05 -0600

    Question

---
+++ algebra742live/models/Checker/__init__.py
@@ -0,0 +1,4 @@
+import json
+
+class Checker(object):
+    pass
--- algebra742live/models/Question/__init__.py
+++ algebra742live/models/Question/__init__.py
@@ -7,14 +7,18 @@ from flask import url_for
 loader = jinja2.FileSystemLoader(os.path.join(os.path.dirname(os.path.abspath(__file__)),"templates"))
 jinja_env = jinja2.Environment(loader=loader)
 
+class AnswerForm(Form):
+    answer = StringField('answer')
+
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     source = db.Column(db.Text)
     params_json = db.Column(db.Text)
 
     def render_html(self):
+        form = AnswerForm()
         template = jinja_env.get_template("{:s}.html".format(self.__class__.__name__))
-        return template.render(json.loads(self.params_json))
+        return template.render(json.loads(self.params_json), form=form)
 
 class PlotQuestion(Question):
     def scripts(self):
