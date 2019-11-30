93dcebb71c3f3cbd4f41e048f5610067f523776a
Chris Durden <cdurden@gmail.com>
2019-11-29 18:59:05 -0600

    Question

--- algebra742live/__init__.py
+++ algebra742live/__init__.py
@@ -48,7 +48,7 @@ def create_app():
         #for json_data in ['{"question": "What is $1+1$?"}', '{"question": "What is 2+1?"}']:
         #    question = get_or_create(db.session, QuestionOnePlusOne, params_json=json_data)
         #    questions.append(question)
-        params = {'parts': [{'class': 'Question.QuestionOnePlusOne', 'params_json': '{"question": "What is $1+1$?"}'}, {'class': 'Question.QuestionOnePlusOne', 'params_json': '{"question": "What is 2+1?"}'}]}
+        params = {'parts': [{'class': '.Question.QuestionOnePlusOne', 'params_json': '{"question": "What is $1+1$?"}'}, {'class': '.Question.QuestionOnePlusOne', 'params_json': '{"question": "What is 2+1?"}'}]}
         question = get_or_create(db.session, MultiPartQuestion, params_json=json.dumps(params))
         questions.append(question)
         ROOMS += [QuestionGame(questions)]
