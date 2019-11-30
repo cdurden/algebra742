7f9ca1b7b6a8fe67e7607bbed32814bb1304a72b
Chris Durden <cdurden@gmail.com>
2019-11-30 15:51:02 -0600

    Question

--- algebra742live/models/Question/PlotQuestion.py
+++ algebra742live/models/Question/PlotQuestion.py
@@ -1,3 +1,4 @@
+from . import Question
 class PlotQuestion(Question):
     def scripts(self):
         return({'canvasjs': "https://canvasjs.com/assets/script/canvasjs.min.js", 'plot': '/static/js/plot.js', 'plotly': 'https://cdn.plot.ly/plotly-latest.min.js'})
