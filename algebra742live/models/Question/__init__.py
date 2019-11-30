a2bde42b807cc6fe51123d588c16c4cf79cf64cf
Chris Durden <cdurden@gmail.com>
2019-11-29 23:34:09 -0600

    Question

--- algebra742live/models/Question/__init__.py
+++ algebra742live/models/Question/__init__.py
@@ -94,7 +94,7 @@ class MultiPartQuestion(Question):
     def check_answer(self, formdata):
         params = self.params()
         form = self.form()
-        return all([part['question'].check_answer(getattr(form,'part_'+str(i)).data for i in range(len(params['parts'])))])
+        return all([part['question'].check_answer(getattr(form,'part_'+str(i)).data for i,part in enumerate(params['parts']))])
 
 
 class QuestionOnePlusOne(Question):
