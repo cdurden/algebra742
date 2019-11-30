6173cec448e544adf72e20ce619f4901513f3939
Chris Durden <cdurden@gmail.com>
2019-11-30 16:01:22 -0600

    Question

--- algebra742live/models/Question/PlotQuestion.py
+++ algebra742live/models/Question/PlotQuestion.py
@@ -1,5 +1,5 @@
 from . import Question
 class PlotQuestion(Question):
     def scripts(self):
-        return({'canvasjs': "https://canvasjs.com/assets/script/canvasjs.min.js", 'plot': '/static/js/plot.js', 'plotly': 'https://cdn.plot.ly/plotly-latest.min.js'})
+        return({'canvasjs': "https://canvasjs.com/assets/script/canvasjs.min.js", 'plot': '/static/js/plot.js', 'plotly': 'https://cdn.plot.ly/plotly-latest.min.js', 'plotly_ex1': '/static/js/plotly_ex1.js'})
 
