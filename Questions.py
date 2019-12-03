67a875debea355bb1ed0b985065b8d4081e643d3
Chris Durden <cdurden@gmail.com>
2019-12-03 07:41:50 -0600

    PlotQuestion

--- Questions.py
+++ Questions.py
@@ -18,6 +18,7 @@ QuestionSets = {
                 {'question': '',
                     'x': json.dumps([1,2,3,4,5,6,7,8,0,0,0,0,0,0,0,0]),
                     'y': json.dumps([0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8])}
+                    'N': 2,
                 ],
             'SpaceAfter': '6cm',
             },
