9a3bfbc1d98c58dee1d6aa245be99f3d2338dc3f
Chris Durden <cdurden@gmail.com>
2019-12-03 08:03:02 -0600

    PlotQuestion

--- Questions.py
+++ Questions.py
@@ -15,9 +15,10 @@ QuestionSets = {
             'Type': 'SetOfCoordinatePairs',
             'Template': 'PlotQuestion.html',
             'ParameterSetVariants': [
-                {'question': '',
+                {'question': 'Plot the linear equation $4x+2y=8$ using the x- and y-intercepts.',
                     'x': json.dumps([1,2,3,4,5,6,7,8,0,0,0,0,0,0,0,0]),
                     'y': json.dumps([0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8]),
+                    'set_of_coordinate_pairs': {(0,4),(2,0)},
                     'N': json.dumps(2),
                 }
             ],
