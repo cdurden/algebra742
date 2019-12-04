165405ab2f29f329f4d9fc46d38d4f724e2e8425
Chris Durden <cdurden@gmail.com>
2019-12-04 00:59:39 -0600

    PlotQuestion

--- algebra742.py
+++ algebra742.py
@@ -874,7 +874,7 @@ def Assignment(lti=lti, assignment=None,q=None,i=None):
                 if simplify(parse_expr(lhs0, transformations=transformations)-parse_expr(rhs0, transformations=transformations))!=0:
                     correct = False
         except:
-            message = "Coordinate pairs could not be read.".format(it+1)
+            message = "Coordinate pairs could not be read."
             correct = False
         answer = json.dumps(form.data)
     if QuestionData['Type'] in ['InputOutputTable']:
