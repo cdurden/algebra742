852675ca1ccf29063a47c33ec1ef3e10af64961b
Chris Durden <cdurden@gmail.com>
2019-12-04 21:33:31 -0600

    PlotQuestion

--- algebra742.py
+++ algebra742.py
@@ -1349,7 +1349,7 @@ def index(lti=lti):
     :return: index page for lti provider
     """
     user = db.session.query(User).filter_by(lti_user_id=lti.name).first()
-    app.logger.error(user.username)
+    #app.logger.error(user.username)
     if user:
         try:
             statement = select([question_scores,Question.__table__]).where(and_(question_scores.c.user_id==user.id, question_scores.c.question_id==Question.__table__.c.id, Question.__table__.c.assignment=='SolvingEquationsTest', Question.__table__.c.number==11, Question.__table__.c.variant_index==0)).order_by(desc('datetime'))
