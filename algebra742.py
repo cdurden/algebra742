bed463df3cb17d821cce35a0275b606b0c5521dd
Chris Durden <cdurden@gmail.com>
2019-12-09 01:56:38 -0600

    GraphingLinearEquationsSkills

--- algebra742.py
+++ algebra742.py
@@ -674,7 +674,7 @@ def Assignment(lti=lti, assignment=None,q=None,i=None):
         try:
             shuffle = Parameters['shuffle']
             input_order = [shuffle[int(re.split("=",x)[1])] for x in re.split("&",form.answers.data)]
-            correct = input_order==Parameters['shuffle']
+            correct = input_order==Parameters['solution']
             app.logger.error(input_order)
             app.logger.error(correct)
         except:
