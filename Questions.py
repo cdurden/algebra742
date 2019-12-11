a59c76b9e8be5a9a84758ee652f3fbfdfb3b3be8
Chris Durden <cdurden@gmail.com>
2019-12-11 05:35:39 -0600

    FlexibleLearningSimulation

--- Questions.py
+++ Questions.py
@@ -33,7 +33,7 @@ QuestionSets = {
                     'dticky': json.dumps(1),
                     'equation': 'y=2x+8',
                     'draw': True,
-                    'html': '<iframe width="560" height="315" src="https://www.youtube.com/embed/CM55FCJJFv4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                    'html': '<div class="video_iframe"><iframe width="560" height="315" src="https://www.youtube.com/embed/CM55FCJJFv4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>',
                  },
                 ],
             'SpaceAfter': '4cm',
