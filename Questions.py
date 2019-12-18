fa01f0bf0f57afc53b458a48d09c8040ad1b69ee
Chris Durden <cdurden@gmail.com>
2019-12-18 02:38:12 -0600

    GraphingLinearEquationsTest

--- Questions.py
+++ Questions.py
@@ -3430,7 +3430,7 @@ for equation in ['y=6x', 'y=-3x', 'y=4/3x', 'y=-1/2x', '3y=2x', '-3y=4x', 'y=-x'
                     'N': json.dumps(0),
                     'equation': 'y = 2x',
                     'x0': '1',
-                    'draw': True,
+#                    'draw': True,
         })
 QuestionSets['DirectVariation']['Questions'].append({
             'Type': 'SetOfCoordinatePairsEquationAndPrediction',
