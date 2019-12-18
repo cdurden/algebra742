5c8a4c8d389bda494261a1de0f46fbd94a5deb80
Chris Durden <cdurden@gmail.com>
2019-12-18 02:21:09 -0600

    GraphingLinearEquationsTest

--- algebra742.py
+++ algebra742.py
@@ -986,9 +986,10 @@ def Assignment(lti=lti, assignment=None,q=None,i=None):
             input_coordinate_pairs = re.split("\)\s*,\s*\(",form.set_of_coordinate_pairs.data)
             input_set_of_coordinate_pairs = set()
             for input_coordinate_pair_string in input_coordinate_pairs:
-                input_coordinate_pair = tuple(float(x.strip("{()} ")) for x in input_coordinate_pair_string.split(","))
-                if len(input_coordinate_pair)!=2:
-                    break
+                try:
+                    input_coordinate_pair = tuple(float(x.strip("{()} ")) for x in input_coordinate_pair_string.split(","))
+                except:
+                    next
                 input_set_of_coordinate_pairs.add(input_coordinate_pair)
                 x = input_coordinate_pair[0]
                 y = input_coordinate_pair[1]
