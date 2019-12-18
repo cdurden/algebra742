a90c05c2174b8016f06dd7e45ef12c3c42451d07
Chris Durden <cdurden@gmail.com>
2019-12-17 22:57:20 -0600

    GraphingLinearEquationsTest

--- static/js/draw.js
+++ static/js/draw.js
@@ -34,6 +34,9 @@ window.addEventListener('load', function () {
       alert('Error: I cannot find the canvas element!');
       return;
     }
+    var rect = canvas_container.getBoundingClientRect();
+    canvaso.width  = rect.width-20;
+    canvaso.height = rect.height-20;
 
     if (!canvaso.getContext) {
       alert('Error: no canvas.getContext!');
@@ -94,16 +97,6 @@ window.addEventListener('load', function () {
     document.addEventListener("touchstart", function (ev) { if (ev.target == canvas) { ev_canvas(ev); return(false); } }, { passive: false });
     document.addEventListener("touchend", function (ev) { if (ev.target == canvas) { ev_canvas(ev); return(false); } }, { passive: false });
 
-    var rect = canvas_container.getBoundingClientRect();
-    context.drawImage(canvaso, 0, 0, canvaso.width, canvaso.height);
-    canvaso.width  = rect.width-20;
-    canvaso.height = rect.height-20;
-    contexto = canvaso.getContext('2d');
-    contexto.drawImage(canvas, 0, 0, canvas.width, canvas.height);
-    canvas.width  = rect.width-20;
-    canvas.height = rect.height-20;
-    context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
     updateImageData();
   }
 
