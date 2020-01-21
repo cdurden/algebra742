994a12271b1fdb8562ea7ab868d8b15064ddbcf6
Chris Durden <cdurden@gmail.com>
2020-01-21 01:22:08 -0600

    interface-dev-with-Question-models

--- algebra742.py
+++ algebra742.py
@@ -695,7 +695,8 @@ def Assignment(lti=lti, assignment=None,q=None,i=None):
         app.logger.error(scripts)
         content = question.render_html()
         if request.method == 'POST':
-            question.build_form(request.form)
+            #question.build_form(request.form)
+            question.build_form()
             app.logger.error(question.form.data)
             correct = question.check_answer()
     if QuestionData['Type'] == 'SubmitAssignment':
