d4ea0e9eb5c775e49548fe012a4a259d7e7417e0
Chris Durden <cdurden@gmail.com>
2020-01-20 23:27:40 -0600

    interface-dev-with-Question-models

--- algebra742live/models/Question/Sort.py
+++ algebra742live/models/Question/Sort.py
@@ -4,3 +4,10 @@ class Sort(Question):
         #return({'jquery': 'https://code.jquery.com/jquery-1.12.4.js', 'sortablejs': 'https://raw.githack.com/SortableJS/Sortable/master/Sortable.js', 'jquery-sortable': 'https://cdn.jsdelivr.net/npm/jquery-sortablejs@latest/jquery-sortable.js'})
         return(['https://code.jquery.com/jquery-1.12.4.js', 'https://raw.githack.com/SortableJS/Sortable/master/Sortable.js', 'https://cdn.jsdelivr.net/npm/jquery-sortablejs@latest/jquery-sortable.js', '/static/js/swap_cards.js'])
 
+    def check_answer(self):
+        params = self.params()
+        shuffle = params['shuffle']
+        input_order = [int(re.split("=",x)[1]) for x in re.split("&",self.form.answers.data)]
+        card_order = [shuffle[i] for i in input_order]
+        return(card_order in params['solutions'])
+
