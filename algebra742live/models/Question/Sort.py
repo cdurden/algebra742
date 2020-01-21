fa8107aec180200984827abd167f69da7bf52454
Chris Durden <cdurden@gmail.com>
2020-01-21 00:24:07 -0600

    interface-dev-with-Question-models

--- algebra742live/models/Question/Sort.py
+++ algebra742live/models/Question/Sort.py
@@ -3,7 +3,11 @@ import re
 class Sort(Question):
     def scripts(self):
         #return({'jquery': 'https://code.jquery.com/jquery-1.12.4.js', 'sortablejs': 'https://raw.githack.com/SortableJS/Sortable/master/Sortable.js', 'jquery-sortable': 'https://cdn.jsdelivr.net/npm/jquery-sortablejs@latest/jquery-sortable.js'})
-        return(['https://code.jquery.com/jquery-1.12.4.js', 'https://raw.githack.com/SortableJS/Sortable/master/Sortable.js', 'https://cdn.jsdelivr.net/npm/jquery-sortablejs@latest/jquery-sortable.js', '/static/js/swap_cards.js'])
+        return(['https://code.jquery.com/jquery-1.12.4.js',
+            'https://raw.githack.com/SortableJS/Sortable/master/Sortable.js',
+            'https://cdn.jsdelivr.net/npm/jquery-sortablejs@latest/jquery-sortable.js',
+            #'/static/js/swap_cards.js'
+            ])
 
     def check_answer(self):
         params = self.params()
