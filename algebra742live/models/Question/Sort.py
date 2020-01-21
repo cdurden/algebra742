5523364c1a18a3d3c65cc2d546b1937ab242e60a
Chris Durden <cdurden@gmail.com>
2020-01-20 23:38:56 -0600

    interface-dev-with-Question-models

--- algebra742live/models/Question/Sort.py
+++ algebra742live/models/Question/Sort.py
@@ -1,4 +1,5 @@
 from . import Question
+import re
 class Sort(Question):
     def scripts(self):
         #return({'jquery': 'https://code.jquery.com/jquery-1.12.4.js', 'sortablejs': 'https://raw.githack.com/SortableJS/Sortable/master/Sortable.js', 'jquery-sortable': 'https://cdn.jsdelivr.net/npm/jquery-sortablejs@latest/jquery-sortable.js'})
