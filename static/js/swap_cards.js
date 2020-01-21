8508d0e90f160337ebcc675c68534f9cb2cfa9d2
Chris Durden <cdurden@gmail.com>
2020-01-20 20:24:04 -0600

    Sort

---
+++ static/js/swap_cards.js
@@ -0,0 +1,27 @@
+options['onAdd'] = function(evt) {
+    var returnItem = $("li",evt.to).eq(evt.newIndex+1);
+    var swapTarget = $(evt.from).children().eq(evt.oldIndex);
+    console.log({
+      'event': name,
+      'this': this,
+      'item': evt.item,
+      'from': evt.from,
+      'to': evt.to,
+      'oldIndex': evt.oldIndex,
+      'newIndex': evt.newIndex,
+      'returnItem': returnItem,
+      'swapTarget': swapTarget,
+    });
+    $(returnItem).remove();
+    if (swapTarget.length != 0) {
+        $(swapTarget).before(returnItem);
+        console.log(swapTarget);
+    } else {
+        $(evt.from).append(returnItem);
+        console.log(swapTarget);
+    }
+}
+$(function() {
+    $("#sortable-blanks").sortable(options);
+    $("#sortable-items").sortable(options);
+});
