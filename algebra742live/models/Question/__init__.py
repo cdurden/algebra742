dd3e46dc45c01049300efe7eee2ce52ea47d22bd
Chris Durden <cdurden@gmail.com>
2020-01-29 01:26:03 -0600

    modeling-jan29

--- algebra742live/models/Question/__init__.py
+++ algebra742live/models/Question/__init__.py
@@ -107,6 +107,7 @@ class MultiPartQuestion(Question):
 #            subform = part['question'].form_class(formdata['part_{:d}'.format(i)])
 #            subform(formdata['part_{:d}'.format(i)])
             #getattr(self.form, 'part_{:d}'.format(i)).bind(self.form, 'part_{:d}'.format(i))
+            print(formdata['part_{:d}'.format(i)])
             getattr(self.form, 'part_{:d}'.format(i)).process(MultiDict(formdata['part_{:d}'.format(i)]))
             #getattr(F, 'part_{:d}'.format(i)).bind(self.form, 'part_{:d}'.format(i)).process(MultiDict(formdata['part_{:d}'.format(i)]))
         #self.form = F(MultiDict(formdata))
