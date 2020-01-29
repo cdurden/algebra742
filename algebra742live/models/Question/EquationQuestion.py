d99a66022f8c27e9484851b53d649c6d1d94202a
Chris Durden <cdurden@gmail.com>
2020-01-28 23:51:42 -0600

    modeling-jan29

--- algebra742live/models/Question/EquationQuestion.py
+++ algebra742live/models/Question/EquationQuestion.py
@@ -1,7 +1,7 @@
 from . import Question
-from sympy import simplify
+from sympy import simplify, groebner
 from sympy.parsing.sympy_parser import parse_expr, standard_transformations, implicit_multiplication_application, convert_xor, split_symbols
-from sympy.polys.groebnertools import groebner
+#from sympy.polys.groebnertools import groebner
 from sympy.polys.rings import ring
 from sympy.polys.domains import ZZ
 from sympy.polys.orderings import lex
@@ -14,7 +14,7 @@ class EquationQuestion(Question):
         input_lhs, input_rhs = [parse_expr(_hs, transformations=transformations) for _hs in self.form.answer.data.split("=")]
         lhs,rhs = [parse_expr(_hs, transformations=transformations) for _hs in params['equation'].split("=")]
         #print(simplify(lhs-rhs+input_lhs-input_rhs))
-        R = ring("x,y", ZZ, lex)
-        print(groebner((lhs-rhs,input_lhs-input_rhs), R))
+        #R = ring("x,y", ZZ, lex)
+        print(groebner([lhs-rhs,input_lhs-input_rhs], x, y, order='lex'))
         correct = simplify(lhs-rhs+input_lhs-input_rhs)==0
         return(correct)
