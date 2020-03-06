9433664664821d004927cdbff7c5f3583c560421
Chris Durden <cdurden@gmail.com>
2020-03-06 00:51:06 -0600

    QuestionDigraphGame

--- static/js/games/reveal.js
+++ static/js/games/reveal.js
@@ -1,11 +1,11 @@
 function output(data) {
     container = document.getElementById('question_'+data.graph+"_"+data.node);
     if (data.correct) {
-      $(container).find('a[class="question_completed"]').attr('data-icon',"check");
-      $(container).find('a[class="question_completed"]').html("Completed");
+      $(container).find('a.question_completed').attr('data-icon',"check");
+      $(container).find('a.question_completed').html("Completed");
     } else {
-      $(container).find('a[class="question_completed"]').attr('data-icon',"alert");
-      $(container).find('a[class="question_completed"]').html("Incomplete");
+      $(container).find('a.question_completed').attr('data-icon',"alert");
+      $(container).find('a.question_completed').html("Incomplete");
     }
 }
 function update_game(data) {
