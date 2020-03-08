a8bc580f758c7782388b091ac313b56cef03b805
Chris Durden <cdurden@gmail.com>
2020-03-07 21:34:47 -0600

    AsyGraphicsQuestion

--- algebra742live/models/util.py
+++ algebra742live/models/util.py
@@ -11,7 +11,8 @@ def asy_params_hash_lookup(params):
     return(hashlib.sha224(json.dumps(params).encode('utf-8')).hexdigest())
 
 def process_quotes_for_json(s):
-    lexer = shlex.shlex(s, posix=True)
+    lexer = shlex.shlex(s)
+    lexer.quotes += "\\\""
     out = ''
     for token in lexer:
         out += "{:s}".format(token.replace("'","\"").replace("\\\"","\""))
