0d99c839a7eee004afcdbdb06ad7d8bf9dc79b46
Chris Durden <cdurden@gmail.com>
2020-03-09 04:36:25 -0500

    reveal-broadcast

--- algebra742live/models/Work.py
+++ algebra742live/models/Work.py
@@ -3,7 +3,7 @@ from .. import db
 
 class Work(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     template = db.Column(db.Text)
     data = db.Column(db.Text)
     user = db.relationship("User", back_populates="works")
