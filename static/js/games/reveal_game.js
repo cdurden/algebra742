ddf4e0add292fc70974e2e8c45713f2bf408fe66
Chris Durden <cdurden@gmail.com>
2020-03-16 18:10:10 -0500

    CompleteTableQuestion

--- static/js/games/reveal_game.js
+++ static/js/games/reveal_game.js
@@ -1,3 +1,20 @@
+function loadScript(url, callback)
+{
+    // adding the script tag to the head as suggested before
+   var head = document.getElementsByTagName('head')[0];
+   var script = document.createElement('script');
+   script.type = 'text/javascript';
+   script.src = url;
+
+   // then bind the event to the callback function 
+   // there are several events for cross browser compatibility
+   script.onreadystatechange = callback;
+   script.onload = callback;
+
+   // fire the loading
+   head.appendChild(script);
+}
+
 function output(data) {
     //container = document.getElementById('question_'+data.graph+"_"+data.node);
     container = $('div.question[source="digraph_question:'+data.graph+":"+data.node+'"]');
