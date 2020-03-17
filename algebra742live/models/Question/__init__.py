259ec7ce3b09e623a86f102e33634282bf8ac714
Chris Durden <cdurden@gmail.com>
2020-03-17 08:37:55 -0500

    CompleteTableQuestion

--- algebra742live/models/Question/__init__.py
+++ algebra742live/models/Question/__init__.py
@@ -31,7 +31,7 @@ class FormField(FormField_):
         self.object_data = data
         prefix = self.name + self.separator
         if isinstance(data, dict):
-            self.form = self.form_class(formdata=formdata, prefix=prefix, **data, csrf_enabled=False)
+            self.form = self.form_class(formdata=formdata, prefix=prefix, csrf_enabled=False, **data)
         else:
             self.form = self.form_class(formdata=formdata, obj=data, prefix=prefix, csrf_enabled=False)
     def traverse_templates(self):
