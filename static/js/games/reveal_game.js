fc9571bad548408560e97372b5d761d783b1fba9
Chris Durden <cdurden@gmail.com>
2020-03-20 03:42:26 -0500

    AlbusDrawingForm

--- static/js/games/reveal_game.js
+++ static/js/games/reveal_game.js
@@ -1,26 +1,26 @@
-  function injectLibFromStack(){
-      if(libs.length > 0){
-        
-        //grab the next item on the stack
-        var nextLib = libs.shift();
-        var headTag = document.getElementsByTagName('head')[0];
-        
-        //create a script tag with this library
-        var scriptTag = document.createElement('script');
-        scriptTag.src = nextLib;
-        
-        //when successful, inject the next script
-        scriptTag.onload = function(e){
-          console.log("---> loaded: " + e.target.src);
-          injectLibFromStack();
-        };    
-        
-        //append the script tag to the <head></head>
-        headTag.appendChild(scriptTag);
-        console.log("injecting: " + nextLib);
-      }
-      else return;
-  }
+function injectLibFromStack(callback){
+    if(libs.length > 0){
+      
+      //grab the next item on the stack
+      var nextLib = libs.shift();
+      var headTag = document.getElementsByTagName('head')[0];
+      
+      //create a script tag with this library
+      var scriptTag = document.createElement('script');
+      scriptTag.src = nextLib;
+      
+      //when successful, inject the next script
+      scriptTag.onload = function(e){
+        console.log("---> loaded: " + e.target.src);
+        injectLibFromStack(callback);
+      };    
+      
+      //append the script tag to the <head></head>
+      headTag.appendChild(scriptTag);
+      console.log("injecting: " + nextLib);
+    }
+    else return callback();
+}
 
 function loadScript(url, callback)
 {
