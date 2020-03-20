7a6bf9ff6673b0a591fea8f1c0e0e4372fbaaf4f
Chris Durden <cdurden@gmail.com>
2020-03-20 03:07:44 -0500

    AlbusDrawingForm

--- static/js/games/reveal_game.js
+++ static/js/games/reveal_game.js
@@ -1,3 +1,27 @@
+  var injectLibFromStack = function(libs){
+      if(libs.length > 0){
+        
+        //grab the next item on the stack
+        var nextLib = libs.shift();
+        var headTag = document.getElementsByTagName('head')[0];
+        
+        //create a script tag with this library
+        var scriptTag = document.createElement('script');
+        scriptTag.src = nextLib;
+        
+        //when successful, inject the next script
+        scriptTag.onload = function(e){
+          console.log("---> loaded: " + e.target.src);
+          injectLibFromStack();
+        };    
+        
+        //append the script tag to the <head></head>
+        headTag.appendChild(scriptTag);
+        console.log("injecting: " + nextLib);
+      }
+      else return;
+  }
+
 function loadScript(url, callback)
 {
     // adding the script tag to the head as suggested before
