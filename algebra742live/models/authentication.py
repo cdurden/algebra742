eb3ae968671d3ae5ea253e9df649f2cc8bbf1c19
Chris Durden <cdurden@gmail.com>
2020-03-21 23:36:44 -0500

    adding-RESTy-api

--- algebra742live/models/authentication.py
+++ algebra742live/models/authentication.py
@@ -1,9 +1,85 @@
-from flask_resty.authentication import *
+import flask
+
+from flask_resty import context
+from flask_resty.exceptions import ApiError
+
+# -----------------------------------------------------------------------------
+
+
+def get_request_credentials():
+    return context.get("request_credentials")
+
+
+def set_request_credentials(credentials):
+    context.set("request_credentials", credentials)
+
+
+# -----------------------------------------------------------------------------
+
+
+class AuthenticationBase:
+    """Base class for API authentication components.
+
+    Authentication components are responsible for extracting the request
+    credentials, if any. They should raise a 401 if the credentials are
+    invalid, but should provide `None` for unauthenticated users.
+
+    Flask-RESTy provides an implementation using `JSON Web Tokens`_  but you
+    can use any authentication component by extending
+    :py:class:`AuthenticationBase` and implementing
+    :py:meth:`get_request_credentials`.
+
+    .. _JSON Web Tokens: https://jwt.io/
+    """
+
+    def authenticate_request(self):
+        """Store the request credentials in the
+        :py:class:`flask.ctx.AppContext`.
+
+        .. warning::
+
+            No validation is performed by Flask-RESTy. It is up to the
+            implementor to validate the request in
+            :py:meth:`get_request_credentials`.
+        """
+        set_request_credentials(self.get_request_credentials())
+
+    def get_request_credentials(self):
+        """Get the credentials for the current request.
+
+        Typically this is done by inspecting :py:data:`flask.request`.
+
+        .. warning::
+
+            Implementing classes **must** raise an exception on authentication
+            failure. A 401 Unauthorized :py:class:`ApiError` is recommended.
+
+        :return: The credentials for the current request.
+        """
+        raise NotImplementedError()
+
+
+# -----------------------------------------------------------------------------
+
+
+class NoOpAuthentication:
+    """An authentication component that provides no credentials."""
+
+    def authenticate_request(self):
+        pass
+
+
+# -----------------------------------------------------------------------------
+
+
 class HeaderAuthenticationBase(AuthenticationBase):
     """Base class for header authentication components.
+
     These authentication components get their credentials from the
     ``Authorization`` request header. The Authorization header has the form::
+
         Authorization: <scheme> <token>
+
     This class also supports fallback to a query parameter, for cases where
     API clients cannot set headers.
     """
@@ -45,6 +121,7 @@ class HeaderAuthenticationBase(AuthenticationBase):
 
     def get_credentials_from_token(self, token):
         """Get the credentials from the token from the request.
+
         :param str token: The token from the request headers or query.
         :return: The credentials from the token.
         """
@@ -53,6 +130,7 @@ class HeaderAuthenticationBase(AuthenticationBase):
 
 class HeaderAuthentication(HeaderAuthenticationBase):
     """Header authentication component where the token is the credential.
+
     This authentication component is useful for simple applications where the
     token itself is the credential, such as when it is a fixed secret shared
     between the client and the server that uniquely identifies the client.
