be8c484051e314ea595ca3ac99dd559658a9b9d2
Chris Durden <cdurden@gmail.com>
2020-03-25 12:01:32 -0500

    adding-snow-qm-task

--- static/js/games/reveal_game.js
+++ static/js/games/reveal_game.js
@@ -80,6 +80,7 @@ function update_snow_qm_task_data(data) {
   //container = document.getElementById('snow-qm_'+data.graph+"_"+data.task);
   container = $('div.question[source="snow-qm:'+data.collection+":"+data.task+'"]');
   console.log(data);
+  console.log(container);
   $(container).find('span[class="question_view"]').html(data.html); 
   form = $(container).find('form');
   $(form).append($(document.createElement('input')).attr('name','collection').attr('type','hidden').val(data.collection));
