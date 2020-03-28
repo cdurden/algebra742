f1f8a8cc6cf081b4d3eedd1feaf4ef49ed6e012f
Chris Durden <cdurden@gmail.com>
2020-03-28 03:42:31 -0500

    inspecting-auth-headers

--- algebra742live/routes.py
+++ algebra742live/routes.py
@@ -149,10 +149,9 @@ def lti_get(lti=lti):
     :return: index page for lti provider
     """
     print(request.headers.get('Authorization'))
-    print(request.headers.get('same-site-cookie'))
-    print(request.headers.get('Set-Cookie'))
-    print(request.headers.get('session'))
-    print(request.headers.get('test'))
+    print(request.cookies.get('same-site-cookie'))
+    print(request.cookies.get('session'))
+    print(request.cookies.get('test'))
     print(session)
     user = db.session.query(User).filter_by(lti_user_id=lti.name).first()
     print(user)
