97f81e6a681a5b65d751c2ceacaa780ff986ffed
Chris Durden <cdurden@gmail.com>
2020-03-27 19:41:08 -0500

    revising-MultiPartQuestion-form

--- algebra742live/models/Question/__init__.py
+++ algebra742live/models/Question/__init__.py
@@ -260,8 +260,8 @@ class MultipleChoiceQuestion(Question):
     form = None
     def build_form(self, formdata=None):
         params = self.params()
-        form.options.choices = range(len(params['choices'])) 
         Question.build_form(self, formdata)
+        self.form.options.choices = range(len(params['choices'])) 
         while len(self.form.entries) < len(self.missing_entries):
             self.form.entries.append_entry()
         return(self.form)
