28ccf022ca36775e48409a1f1f909b741e5f7256
Chris Durden <cdurden@gmail.com>
2020-03-30 04:38:21 -0500

    chat

--- algebra742live/routes.py
+++ algebra742live/routes.py
@@ -91,6 +91,7 @@ def render_snow_qm_task(collection_id=None,task_id=None):
 @socketio.on('chat-message')
 @lti(request='session', error=error)
 def handle_chat_message(data, lti=lti):
+    print("got message")
     try:
         user = db.session.query(User).filter_by(lti_user_id=lti.name).first()
     except:
