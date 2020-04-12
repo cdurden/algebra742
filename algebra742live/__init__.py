99bfa36cef7ff8472e2a6851ebda205cd81958af
Chris Durden <cdurden@gmail.com>
2020-04-12 18:42:47 -0500

    using JSONEncoder

--- algebra742live/__init__.py
+++ algebra742live/__init__.py
@@ -69,7 +69,7 @@ class AlchemyEncoderMiddleWare(object):
     def __init__(self, app):
         self.app = app
     def __call__(self, environ, start_response):
-        app.extensions['restful'].representations.json.settings["cls"] = new_alchemy_encoder() 
+        self.app.extensions['restful'].representations.json.settings["cls"] = new_alchemy_encoder() 
         return self.app(environ, start_response)
 
 #def new_alchemy_encoder(revisit_self = False, fields_to_expand = []):
