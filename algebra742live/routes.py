ba04f308784ab05ae000b6bb77487dba9018eb85
Chris Durden <cdurden@gmail.com>
2020-04-18 13:59:21 -0500

    get latest board

--- algebra742live/routes.py
+++ algebra742live/routes.py
@@ -224,6 +224,7 @@ from . import models
 from models.User import get_user_by_lti_user_id
 from models.Task import get_tasks, get_task_by_id, get_task_from_source, get_task_data_by_source_pattern
 from models.Submission import get_submission_by_id, get_submissions
+from models.Board import get_boards, get_board_by_id, get_latest_board
 from models.util import SerializableGenerator
 import flask_restful.representations.json
 
