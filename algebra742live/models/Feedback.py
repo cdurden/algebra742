b7d612eb55ce49310adc10d61e7419af6dff965d
Chris Durden <cdurden@gmail.com>
2020-04-20 18:52:21 -0500

    feedback

--- algebra742live/models/Feedback.py
+++ algebra742live/models/Feedback.py
@@ -8,18 +8,20 @@ from sqlalchemy.orm import relationship
 class Feedback(db.Model):
     __tablename__ = 'feedback'
     id = db.Column(db.Integer, primary_key=True)
+    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     board_id = db.Column(db.Integer, db.ForeignKey('board.id'))
     submission_id = db.Column(db.Integer, db.ForeignKey('submission.id'))
     task_id = db.Column(db.Integer, db.ForeignKey('submission.id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     data = db.Column(db.Text)
     datetime = db.Column(db.DateTime, nullable=False,
                     default=datetime.utcnow)
 
-    submission = relationship("Submission", back_populates="feedback")
-    user = relationship("User", back_populates="feedback")
-    task = relationship("Task", back_populates="feedback")
-    board = relationship("Board", back_populates="feedback")
+    creator = relationship("User", foreign_keys=[creator_id], back_populates="feedback")
+    recipient = relationship("User", foreign_keys=[recipient_id], back_populates="feedback")
+    board = relationship("Board", foreign_keys=[board_id], back_populates="feedback")
+    submission = relationship("Submission", foreign_keys=[submission_id], back_populates="feedback")
+    task = relationship("Task", foreign_keys=[task_id], back_populates="feedback")
 
 def get_feedback(**kwargs):
     feedback = db.session.query(Feedback).filter_by(**kwargs).all()
