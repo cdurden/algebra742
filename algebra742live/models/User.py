15cf23bdc98fa73a6c1eb960b8f0763dfdf8e442
Chris Durden <cdurden@gmail.com>
2020-04-20 20:29:26 -0500

    feedback

--- algebra742live/models/User.py
+++ algebra742live/models/User.py
@@ -43,11 +43,14 @@ class User(db.Model):
         db.session.commit()
         return(board)
     def create_feedback(self, users, tasks, submissions):
-        board = db.Board(user_id=self.id, data_json=json.dumps(data))
-        feedback = db.Feedback(board=board)
-        feedback.users = users
-        feedback.tasks = tasks
-        feedback.submissions = submissions
+
+        for user in users:
+            for task in tasks:
+                board = db.Board(user_id=self.id, data_json=json.dumps(data))
+                feedback = db.Feedback(board=board, user=user, task=task)
+        #if submissions: 
+        #    feedback = db.Feedback(board=board, user=user, task=task)
+        #feedback.submissions = submissions
         db.session.add(feedback)
         db.session.add(board)
         db.session.commit()
