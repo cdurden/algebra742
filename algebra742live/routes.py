ba1c7dfe5b755e650acb43d6afda12462739e0cc
Chris Durden <cdurden@gmail.com>
2020-05-09 20:37:22 -0500

    -m

--- algebra742live/routes.py
+++ algebra742live/routes.py
@@ -463,12 +463,15 @@ class Submission(Resource):
 
 class SubmissionList(Resource):
     @api_authenticate
-    def get(self):
+    def get(self, state):
         parser = reqparse.RequestParser()
         parser.add_argument('data')
         args = parser.parse_args()
         kwargs = args['data'] or {}
-        submissions = get_submissions(**kwargs)
+        if state is None or state == '':
+            submissions = get_submissions(**kwargs)
+        else:
+            submissions = get_submissions(state=state, **kwargs)
         return submissions_schema.dump(submissions)
         #return([submission.to_json() for submission in get_submissions(**kwargs)])
 
@@ -698,7 +701,8 @@ api.add_resource(SourcedTaskBoardList, "/api/tasks/source/boards/")
 api.add_resource(TaskDataList, "/api/tasks/data/<source_pattern>/")
 api.add_resource(Submission, "/api/submission/<submission_id>")
 api.add_resource(TaskSubmissionList, "/api/task/<task_id>/submissions/")
-api.add_resource(SubmissionList, "/api/submissions/")
+api.add_resource(SubmissionList, "/api/submissions/", state=None)
+api.add_resource(SubmissionList, "/api/submissions/<state>")
 api.add_resource(Board, "/api/user/<lti_user_id>/board/<boardId>")
 api.add_resource(LatestBoard, "/api/board/")
 api.add_resource(BoardList, "/api/boards/")
